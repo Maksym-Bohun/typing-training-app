010eba03507dd022aaee724679d136074f4ead4c
Maksym Bohun <maks.boggun@gmail.com>
2023-03-06 14:21:27 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import "./App.css";
+import Home from "./components/pages/Home";
+import { Root } from "./components/pages/RootLayout";
+import { SpeedTest } from "./components/speed-test/SpeedTest";
+import { Training } from "./components/pages/Training";
+import { Challenge } from "./components/pages/Challenge";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "actions",
+        element: <Root />,
+        children: [
+          { path: "test", element: <SpeedTest /> },
+          { path: "training", element: <Training /> },
+          { path: "challenge", element: <Challenge /> },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
